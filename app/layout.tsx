a752f53410ba0cba39cc59ce68a6265887fe8c2d
AcidicOnionas <104092113+AcidicOnionas@users.noreply.github.com>
2025-03-20 09:09:52 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,29 +40,31 @@ export default function RootLayout({
                 </Link>
               </nav>
               <div className="flex items-center gap-2">
-                <ModeToggle />
-                {/* <Button asChild variant="default" size="sm" className="hidden md:flex">
-                  <Link href="/about#contact">About Me</Link>
-                </Button>
-                <Button variant="outline" size="icon" className="md:hidden">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="h-5 w-5"
-                  >
-                    <line x1="4" x2="20" y1="12" y2="12" />
-                    <line x1="4" x2="20" y1="6" y2="6" />
-                    <line x1="4" x2="20" y1="18" y2="18" />
-                  </svg>
-                  <span className="sr-only">Toggle menu</span>
-                </Button> */}
+                <div className="mr-4">
+                    <ModeToggle />
+                    {/* <Button asChild variant="default" size="sm" className="hidden md:flex">
+                    <Link href="/about#contact">About Me</Link>
+                    </Button> */}
+                    <Button variant="outline" size="icon" className="md:hidden">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="h-5 w-5"
+                    >
+                        <line x1="4" x2="20" y1="12" y2="12" />
+                        <line x1="4" x2="20" y1="6" y2="6" />
+                        <line x1="4" x2="20" y1="18" y2="18" />
+                    </svg>
+                    <span className="sr-only">Toggle menu</span>
+                    </Button>
+                </div>
               </div>
             </div>
           </header>
