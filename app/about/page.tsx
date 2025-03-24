ea2a2bf39587e9b4eaa76894db178b18bba39321
AcidicOnionas <104092113+AcidicOnionas@users.noreply.github.com>
2025-03-23 17:27:29 -0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -38,7 +38,7 @@ export default function AboutPage() {
                     </Link>
                   </Button> */}
                   <Button asChild variant="outline" size="icon">
-                    <Link href="https://github.com" target="_blank" rel="noopener noreferrer">
+                    <Link href="https://github.com/AcidicOnionas" target="_blank" rel="noopener noreferrer">
                       <Github className="h-4 w-4" />
                       <span className="sr-only">GitHub</span>
                     </Link>
